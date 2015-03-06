6467150f7a6a82c55e637c5def925eb3f561c8dc
Tom Spink <t.spink@sms.ed.ac.uk>
2015-03-06 12:29:41 +0000

    Initial Commit

---
+++ src/uk/ac/ed/inf/op/Program.java
@@ -0,0 +1,17 @@
+/*
+ * Program.java
+ * 
+ * Tom Spink <t.spink@sms.ed.ac.uk>
+ * University of Edinburgh 2015
+ */
+package uk.ac.ed.inf.op;
+
+public class Program {
+	/**
+	 * Application entry-point.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world.");
+	}
+}
