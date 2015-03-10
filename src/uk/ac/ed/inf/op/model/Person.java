925d535a22d662178982cc89c0bbe593b854f6f8
Kyriakosk <kyriakosk5@gmail.com>
2015-03-10 22:29:51 +0000

    Update Person.java

--- src/uk/ac/ed/inf/op/model/Person.java
+++ src/uk/ac/ed/inf/op/model/Person.java
@@ -1,5 +1,12 @@
 package uk.ac.ed.inf.op.model;
 
 public class Person {
-	public String firstName;
+	private String fstName;
+	private String lstName;
+	private String email;
+	public Person(String fstName, String lstName, email){
+		this.fstName = fstName;
+		this.lstName = lstName;
+		
+	}
 }
